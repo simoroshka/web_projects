b00da433aab2191738d10d85f0778d7692f4d592
Simoroshka <simoroshka@gmail.com>
2016-06-01 18:54:14 +0300

    update fav-movies to the current version

--- fav-movies/index.php
+++ fav-movies/index.php
@@ -1,19 +1,21 @@
 <?php
 
-require_once 'php-includes/get-variables.inc.php';
-require_once 'php-includes/head.inc.php';
-require_once 'php-includes/header.inc.php';
-require_once 'php-includes/navigation.inc.php';
-require_once 'php-includes/favourites.inc.php';
+set_include_path('./php-includes' . PATH_SEPARATOR . './functions');
 
+// Functions
+require_once 'test-users.fn.php';
+require_once 'show-users.fn.php';
+require_once 'test-movies.fn.php';
+require_once 'show-movies.fn.php';
+require_once 'test-fav.fn.php';
 
+// Includes
+require_once 'connect.inc.php';
+require_once 'get-variables.inc.php';
+require_once 'head.inc.php';
+require_once 'header.inc.php';
+require_once 'navigation.inc.php';
+require_once 'main.inc.php';
+require_once 'footer.inc.php';
 
-if (isset($movieID)) {
-	require_once 'php-includes/movie-single.inc.php';
-}
-else {
-	require_once 'php-includes/movie-list.inc.php';
-}
-
-require_once 'php-includes/footer.inc.php';
 ?>
