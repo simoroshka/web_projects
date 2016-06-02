da892dc03af5174a56c66139e56893b0d2eeacbf
Simoroshka <simoroshka@gmail.com>
2016-06-02 18:52:15 +0300

    improve the input form

--- weather app/assets/javascript/controllers/homeController.js
+++ weather app/assets/javascript/controllers/homeController.js
@@ -1,7 +1,11 @@
 angular.module("WeatherApp")
-.controller('HomeController', ['$scope', 'cityService', function($scope, cityService){
+.controller('HomeController', ['$scope', '$location', 'cityService',  function($scope, $location, cityService){
     $scope.city = cityService.city;
     $scope.$watch('city', function() {
         cityService.city = $scope.city;
     });
+    
+    $scope.submit = function() {
+        $location.path("/forecast");   
+    }
 }]);
